69fdceb2f2073361e137f6935ec19458d5a6b0aa
Andika Putra Wijaya <128115929+DikaWasHere@users.noreply.github.com>
2024-09-20 20:52:07 +0700

    Add files via upload

---
+++ setTimeout.js
@@ -0,0 +1,12 @@
+//dijalankan di terminal
+console.log("halo dunia");
+
+setTimeout(function () {
+  console.log("halu dunia 2");
+}, 1000);
+
+setTimeout(() => {
+  console.log("isekai koh");
+}, 3);
+
+console.log("halo dunia 3");
